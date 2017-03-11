18d5d90db6f7c3ddf240fe0c0bd72cd75ad012e8
Jeff Corcoran <jcorcoran+github@gmail.com>
2017-03-11 00:18:17 -0500

    Response needs to be public

--- oauth2library/src/main/java/ca/mimic/oauth2library/OAuthResponse.java
+++ oauth2library/src/main/java/ca/mimic/oauth2library/OAuthResponse.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 import okhttp3.Response;
 
-class OAuthResponse {
+public class OAuthResponse {
     private Response response;
     private String responseBody;
     private Token token;
