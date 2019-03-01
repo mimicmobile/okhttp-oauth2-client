9f2584ae22aa0172c46d93f8923ad45e60ff9e86
Alessio Moiso <a.moiso@pillohealth.com>
2019-03-01 16:28:04 +0100

    Improve OAuth2Client interface

--- oauth2library/src/main/java/com/pillohealth/oauth2library/OAuth2Client.kt
+++ oauth2library/src/main/java/com/pillohealth/oauth2library/OAuth2Client.kt
@@ -4,9 +4,7 @@ package com.pillohealth.oauth2library
 public interface OAuth2Client {
 
     fun requestAccessToken(): OAuthResponse
-    fun requestAccessToken(callback: OAuthResponseCallback)
 
     fun refreshAccessToken(refreshToken: String): OAuthResponse
-    fun refreshAccessToken(refreshToken: String, callback: OAuthResponseCallback)
 
 }
