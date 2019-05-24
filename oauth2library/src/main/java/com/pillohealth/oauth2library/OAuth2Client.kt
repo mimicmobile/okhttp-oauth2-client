2a6e0a6db7fb191aef21616464359dda203a3b5a
Alessio Moiso <a.moiso@pillohealth.com>
2019-05-24 15:53:09 +0200

    Add throws annotation in client

--- oauth2library/src/main/java/com/pillohealth/oauth2library/OAuth2Client.kt
+++ oauth2library/src/main/java/com/pillohealth/oauth2library/OAuth2Client.kt
@@ -1,10 +1,14 @@
 package com.pillohealth.oauth2library
 
+import java.io.IOException
+
 @Suppress("RedundantVisibilityModifier")
 public interface OAuth2Client {
 
+    @Throws(IOException::class)
     fun requestAccessToken(): OAuthResponse
 
+    @Throws(IOException::class)
     fun refreshAccessToken(refreshToken: String): OAuthResponse
 
 }
