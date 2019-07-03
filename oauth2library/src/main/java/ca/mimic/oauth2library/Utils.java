247703775cca1b9c693d16c8bc6c381ec6f44951
Avishai Gurt <avishai.gurt@oviva.com>
2019-07-03 11:57:56 +0200

    Fix NPE in case the content-type is empty in the response

--- oauth2library/src/main/java/ca/mimic/oauth2library/Utils.java
+++ oauth2library/src/main/java/ca/mimic/oauth2library/Utils.java
@@ -14,7 +14,7 @@ import okhttp3.Route;
 class Utils {
 
     protected static boolean isJsonResponse(Response response) {
-        return response.body() != null && response.body().contentType().subtype().equals("json");
+        return response.body() != null && response.body().contentType() != null && response.body().contentType().subtype().equals("json");
     }
 
     protected static Authenticator getAuthenticator(final OAuth2Client oAuth2Client,
